7ae61577dc6ef944b0bf3d1aadf5193910774e5b
Tomas Stolker <tomas.stolker@phys.ethz.ch>
2019-03-14 22:30:37 +0100

    docs

--- docs/source/conf.py
+++ docs/source/conf.py
@@ -15,8 +15,8 @@
 import os
 import sys
 
-sys.path.insert(0, os.path.abspath('../../'))
-autodoc_mock_imports = ['species']
+sys.path.insert(0, os.path.abspath('./../../'))
+# autodoc_mock_imports = ['species']
 
 
 # -- Project information -----------------------------------------------------
