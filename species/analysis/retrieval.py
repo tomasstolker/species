a4d3296e4ab760862372275129ccfb96d2abe9f4
Tomas Stolker <tomas.stolker@phys.ethz.ch>
2020-08-15 19:36:34 +0200

    Cube transformation of the extinction parameters

--- species/analysis/retrieval.py
+++ species/analysis/retrieval.py
@@ -793,6 +793,24 @@ class AtmosphericRetrieval:
                         (bounds[f'corr_amp_{item}'][1]-bounds[f'corr_amp_{item}'][0]) * \
                         cube[cube_index[f'corr_amp_{item}']]
 
+            # ISM extinction
+
+            if 'ism_ext' in bounds:
+                ism_ext = bounds['ism_ext'][0] + (bounds['ism_ext'][1]-bounds['ism_ext'][0])*cube[cube_index['ism_ext']]
+            else:
+                # default: 0 - 5 mag
+                ism_ext = 5.*cube[cube_index['ism_ext']]
+
+            cube[cube_index['ism_ext']] = ism_ext
+
+            if 'ism_red' in bounds:
+                ism_red = bounds['ism_red'][0] + (bounds['ism_red'][1]-bounds['ism_red'][0])*cube[cube_index['ism_red']]
+            else:
+                # default: 0 - 5
+                ism_red = 5.*cube[cube_index['ism_red']]
+
+            cube[cube_index['ism_red']] = ism_ext
+
         @typechecked
         def loglike(cube,
                     n_dim: int,
