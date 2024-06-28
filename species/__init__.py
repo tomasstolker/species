90ca7925d354a3319ee3467d1d1108e00ce979d8
Tomas Stolker <stolker@strw.leidenuniv.nl>
2024-06-28 09:10:21 +0200

    v0.8.4

--- species/__init__.py
+++ species/__init__.py
@@ -2,7 +2,7 @@ from species.core.species_init import SpeciesInit
 
 __author__ = "Tomas Stolker"
 __license__ = "MIT"
-__version__ = "0.8.3"
+__version__ = "0.8.4"
 __maintainer__ = "Tomas Stolker"
 __email__ = "stolker@strw.leidenuniv.nl"
 __status__ = "Development"
