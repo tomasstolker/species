985ad874eee03848cdec33c4df9f0900adcf6ff9
Tomas Stolker <stolker@strw.leidenuniv.nl>
2025-09-08 11:58:02 +0200

    Updated version

--- species/_version.py
+++ species/_version.py
@@ -28,7 +28,7 @@ version_tuple: VERSION_TUPLE
 commit_id: COMMIT_ID
 __commit_id__: COMMIT_ID
 
-__version__ = version = '0.9.1.dev93+g8ff86bf3d.d20250908'
-__version_tuple__ = version_tuple = (0, 9, 1, 'dev93', 'g8ff86bf3d.d20250908')
+__version__ = version = '0.10.0'
+__version_tuple__ = version_tuple = (0, 10, 0)
 
-__commit_id__ = commit_id = 'g8ff86bf3d'
+__commit_id__ = commit_id = None
